ad801a3d614c9ad5589413e16fccb7c224f3200c
mgstabrani <mgstabrani19@gmail.com>
2020-12-30 06:40:55 +0700

    First commit

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model {
+    private $nama = 'Mgs. Tabrani';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="jumbotron mt-4">
+      <h1 class="display-4">Selamat Datang!</h1>
+      <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
+      <hr class="my-4">
+    </div>
+
+
+</div>
