7a9490a45ac92e3416f38fd24d8e37fe8b25f03c
mgstabrani <mgstabrani19@gmail.com>
2020-12-30 06:53:47 +0700

    Adding readme and importing database

---
+++ README.md
@@ -0,0 +1,19 @@
+# digitiket-intern
+
+## Requirements
+- PHP versi 7 ke atas. 
+- Software XAMPP.
+- Web Browser
+
+## Cara menjalankan
+- Clone project ini ke dalam folder htdocs di XAMPP
+```
+cd C:/xampp/htdocs/
+```
+- Buka web browser, dan jalankan url berikut 
+```
+http://localhost/digitiket-intern/public/
+```
+
+## Setup Database
+- Sesuaikan database dengan config yang ada di app/config/config.php atau bisa langsung mengimpor dari file digitiket.sql.
---
+++ digitiket.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Des 2020 pada 00.51
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.3.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `digitiket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nrp` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`) VALUES
+(1, 'Mgs. Tabrani', '13519122', 'mgstabrani19@gmail.com', 'Teknik Informatika'),
+(2, 'Joko Widodo', '16519101', 'jokowidodo@gmail.com', 'Teknik Planologi'),
+(3, 'Bambang Susilo ', '14519100', 'bambangsusilo@gmail.com', 'Teknik Industri'),
+(4, 'Rudi Habibie', '12519102', 'rudihabibie@gmail.com', 'Teknik Mesin'),
+(5, 'Abdul Wahid', '17519111', 'abdulwahid@gmail.com', 'Teknik Lingkungan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
